bc8242da64120b387bb13fa81f40b05cb31ce1ff
binghan60 <binghan60@gmail.com>
2022-08-30 21:22:48 +0800

    rating star

---
+++ src/pages/ProductList/components/Rating.js
@@ -0,0 +1,69 @@
+function Rating(props) {
+  const { rating, numReviews } = props;
+  return (
+    <div className="d-flex justify-content-between">
+      <div className="rating">
+        <span>
+          <i
+            className={
+              rating >= 1
+                ? "fas fa-star"
+                : rating >= 0.5
+                ? "fas fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            className={
+              rating >= 2
+                ? "fas fa-star"
+                : rating >= 1.5
+                ? "fas fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            className={
+              rating >= 3
+                ? "fas fa-star"
+                : rating >= 2.5
+                ? "fas fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            className={
+              rating >= 4
+                ? "fas fa-star"
+                : rating >= 3.5
+                ? "fas fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            className={
+              rating >= 5
+                ? "fas fa-star"
+                : rating >= 4.5
+                ? "fas fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        </span>
+      </div>
+      <span>{numReviews}人也看過</span>
+    </div>
+
+  );
+}
+
+
+export default Rating
