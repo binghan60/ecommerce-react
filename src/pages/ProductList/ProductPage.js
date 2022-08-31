230c00b5e609027c60c763ca75b19290abdfa991
binghan60 <binghan60@gmail.com>
2022-08-31 16:41:56 +0800

    產品內頁

--- src/pages/ProductList/ProductPage.js
+++ src/pages/ProductList/ProductPage.js
@@ -1,11 +1,19 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Rating from "./components/Rating";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import Badge from "react-bootstrap/Badge";
 import axios from "axios";
+import { Helmet } from "react-helmet-async";
 
 function ProductPage() {
   const params = useParams();
   const { slug } = params;
   const [ProductPageData, setProductPageData] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,10 +24,70 @@ function ProductPage() {
     };
     fetchData();
   }, [slug]);
+
+
+
   return (
     <>
-      <div>
-        <img src={`/imgs/${ProductPageData.img}`} alt="" />
+      <Row>
+        {console.log(ProductPageData.id)}
+        <Col sm={12} md={6} lg={6}>
+          <img
+            className="w-100"
+            src={`/imgs/${ProductPageData.img}`}
+            alt={ProductPageData.name}
+          />
+        </Col>
+        <Col sm={12} md={6} lg={6}>
+          <ListGroup>
+            <ListGroup.Item>
+              {/* 網頁標題 */}
+              <Helmet>
+                <title className="text-center">{ProductPageData.name}</title>
+              </Helmet>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              評價
+              <Rating
+                rating={ProductPageData.rating}
+                numReviews={ProductPageData.numReviews}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              商品描述：
+              <p className="text-center">{ProductPageData.description}</p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              產品狀態：
+              {ProductPageData.countInStock > 0 ? (
+                <Badge bg="success">庫存充足</Badge>
+              ) : (
+                <Badge bg="danger">售完</Badge>
+              )}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              價格：
+              <p className="text-center">{ProductPageData.price}元</p>
+            </ListGroup.Item>
+            {ProductPageData.countInStock > 0 ? (
+              <ListGroup.Item>
+                <div className="d-grid">
+                  <Button variant="success">加入購物車</Button>
+                </div>
+              </ListGroup.Item>
+            ) : (
+              <ListGroup.Item>
+                <div className="d-grid">
+                  <Button variant="danger" disabled>
+                    商品補貨中
+                  </Button>
+                </div>
+              </ListGroup.Item>
+            )}
+          </ListGroup>
+        </Col>
+      </Row>
+      <div className="text-center">
         <Link to={"/ProductList"}>
           <button className="btn btn-light">回商品列表</button>
         </Link>
