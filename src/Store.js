bc1bea2400afd5fcfbff67ac0bf404cc1059df2f
binghan60 <binghan60@gmail.com>
2022-09-05 04:18:28 +0800

    cart to localstorage

--- src/Store.js
+++ src/Store.js
@@ -4,7 +4,10 @@ export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
+    // localStorage沒東西就是空陣列
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
   },
 };
 
@@ -21,11 +24,14 @@ function reducer(state, action) {
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     case "CART_REMOVE_ITEM": {
+      //移除商品 filter沒有payload(自己)的陣列
       const cartItems = state.cart.cartItems.filter(
         (item) => item._id !== action.payload._id
       );
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
 
