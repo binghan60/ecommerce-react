80747caf1fff2afc4446a85b3eda2f3cda1b541b
binghan60 <binghan60@gmail.com>
2022-09-05 04:18:44 +0800

    加入購物車按鈕

--- src/pages/ProductList/components/ProductCard.js
+++ src/pages/ProductList/components/ProductCard.js
@@ -1,8 +1,31 @@
+import { useContext } from "react";
+import { Store } from "../../../Store";
 import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import axios from "axios";
 import Rating from "./Rating";
 
 function ProductCard(props) {
   const { ProductData } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === ProductData._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(
+      `http://localhost:5000/api/products/${item._id}`
+    );
+    if (data.countInStock < quantity) {
+      window.alert("商品庫存不足");
+      return;
+    }
+    ctxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...item, quantity },
+    });
+  };
   return (
     <div>
       <Link to={`/ProductList/${ProductData.slug}`}>
@@ -17,6 +40,18 @@ function ProductCard(props) {
         <strong>{ProductData.price}元</strong>
         <span className="float-end">庫存尚有{ProductData.countInStock}份</span>
       </p>
+      <div className="text-center">
+        {ProductData.countInStock === 0 ? (
+          <Button variant="danger" disabled>商品補貨中</Button>
+        ) : (
+          <Button
+            onClick={() => addToCartHandler(ProductData)}
+            variant="success"
+          >
+            加入購物車
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
