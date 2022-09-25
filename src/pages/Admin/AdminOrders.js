8381bddc1bd173c8e05af05c899d1b2beab4aadd
binghan60 <binghan60@gmail.com>
2022-09-26 00:39:11 +0800

    訂單列表

--- src/pages/Admin/AdminOrders.js
+++ src/pages/Admin/AdminOrders.js
@@ -3,6 +3,7 @@ import { useContext, useEffect, useReducer } from "react";
 import { Button } from "react-bootstrap";
 import { Helmet } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import LoadingBox from "../../components/LoadingBox";
 import { Store } from "../../Store";
 
@@ -14,6 +15,14 @@ const reducer = (state, action) => {
       return { ...state, orders: action.payload, loading: false };
     case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true, successDelete: false };
+    case "DELETE_SUCCESS":
+      return { ...state, loadingDelete: false, successDelete: true };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false };
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
     default:
       return state;
   }
@@ -23,10 +32,11 @@ function AdminOrders() {
   const navigate = useNavigate();
   const { state } = useContext(Store);
   const { userInfo } = state;
-  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
-    loading: true, //初始載入  失敗或成功都會變false
-    error: "",
-  });
+  const [{ loading, error, orders, loadingDelete, successDelete }, dispatch] =
+    useReducer(reducer, {
+      loading: true, //初始載入  失敗或成功都會變false
+      error: "",
+    });
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -39,19 +49,40 @@ function AdminOrders() {
         dispatch({ type: "FETCH_FAIL", payload: "取得訂單列表失敗" });
       }
     };
-    fetchData();
-  }, [userInfo]);
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    } //成功刪除 改變successDelete 重跑一次useEffect 更新資料 並重置刪除狀態
+  }, [userInfo, successDelete]);
+
+  const deleteHandler = async (order) => {
+    if (window.confirm("確定要刪除嗎?")) {
+      try {
+        dispatch({ type: "DELETE_REQUEST" });
+        await axios.delete(`http://localhost:5000/api/orders/${order._id}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: "DELETE_SUCCESS" });
+        toast.success("刪除成功");
+      } catch (err) {
+        dispatch({ type: "DELETE_FAIL" });
+        toast.error("刪除失敗");
+      }
+    }
+  };
 
   return (
     <>
       <Helmet>訂單管理</Helmet>
       <h3>訂單管理</h3>
+      {loadingDelete && <LoadingBox></LoadingBox>}
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
         "發生錯誤"
       ) : (
-        <table className="table table-dark">
+        <table className="table table-dark text-center">
           <thead>
             <tr>
               <th>ID</th>
@@ -80,6 +111,7 @@ function AdminOrders() {
                 </td>
                 <td>
                   <Button
+                    className="mx-1"
                     type="button"
                     variant="light"
                     onClick={() => {
@@ -87,6 +119,14 @@ function AdminOrders() {
                     }}
                   >
                     訂單明細
+                  </Button>
+                  <Button
+                    onClick={() => deleteHandler(order)}
+                    className="mx-1"
+                    type="button"
+                    variant="light"
+                  >
+                    刪除
                   </Button>
                 </td>
               </tr>
