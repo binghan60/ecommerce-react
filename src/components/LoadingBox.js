b282d964b247432b22bc893a892350d490f00512
binghan60 <binghan60@gmail.com>
2022-09-28 23:54:43 +0800

    LoadingBox

--- src/components/LoadingBox.js
+++ src/components/LoadingBox.js
@@ -1,5 +1,18 @@
+import { Blocks } from "react-loader-spinner";
+
 function LoadingBox() {
-  return <>載入中</>;
+  return (
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
+      <Blocks
+        visible={true}
+        height="100"
+        width="100"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+      />
+    </div>
+  );
 }
 
 export default LoadingBox;
